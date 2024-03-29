7ade5cbaf58d7b616a82ee6dbb8404ae2eedc3e1
unknown <kumaradarsh921@gmail.com>
2024-03-29 17:23:25 +0530

    Git Auto push

---
+++ env-prod/prod-backend.tfvars
@@ -0,0 +1,3 @@
+    bucket = "b55-tf-state"
+    key    = "databases/prod/terraform.tfstate"
+    region = "us-east-1"
---
+++ main.tf
@@ -0,0 +1,9 @@
+module "docdb" {
+    source              = "./vendor/modules/docdb"
+    ENV                 = var.ENV
+
+}
+
+# We cannot parameterize anything that is added in the source
+
+# To limit that, we can use a tool called as Terrafile. All it does is clones the remote code from the specified branch and make it locally available. 
