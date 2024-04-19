fcc474eba71a461a13039cec883dd4682ada8aef
unknown <kumaradarsh921@gmail.com>
2024-04-19 20:47:11 +0530

    Git Auto push

--- main.tf
+++ main.tf
@@ -11,10 +11,20 @@ module "redis" {
 }
 
 module "mysql" {
-    source              = "./vendor/modules/mysql"
-    ENV                 = var.ENV
+    source               = "./vendor/modules/mysql"
+    ENV                  = var.ENV
+    MYSQL_STORAGE        = var.MYSQL_STORAGE
+    MYSQL_ENGINE_VERSION = var.MYSQL_ENGINE_VERSION
+    MYSQL_INSTANCE_TYPE  = var.MYSQL_INSTANCE_TYPE
 
 }
+
+
+# module "rabbitmq" {
+#     source              = "./vendor/modules/rabbitmq"
+#     ENV                 = var.ENV
+
+# }
 # We cannot parameterize anything that is added in the source
 
 # To limit that, we can use a tool called as Terrafile. All it does is clones the remote code from the specified branch and make it locally available. 
